48d7be2cd788825f83153055c582e2999d4684a6
Ywur7t(Martyn_Vlad) <m4rtyn.v@yandex.ru>
2024-11-11 14:21:44 +1000

    с

---
+++ blogicum/blog/models.py
@@ -0,0 +1,34 @@
+# blog/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="posts")
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    content = models.TextField()
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    date_commented = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
---
+++ blogicum/pages/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def rules(request):
+    return render(request, 'pages/rules.html')
